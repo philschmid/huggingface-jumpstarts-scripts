ea452f62ddfcb3cded4fad04688e0bdfee92446a
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 14:18:03 +0000

    Adding tarfile member sanitization to extractall()

--- spc/sentence_pair_classification_transfer_learning.py
+++ spc/sentence_pair_classification_transfer_learning.py
@@ -75,7 +75,26 @@ def _get_model_and_tokenizer(args):
     # extract model files
     tarball_extract_dir_name = constants.DOWNLOADED_MODEL_NAME.replace(constants.DOT_TAR_GZ, "")
     with tarfile.open(constants.DOWNLOADED_MODEL_NAME) as saved_model_tar:
-        saved_model_tar.extractall(tarball_extract_dir_name)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(saved_model_tar, tarball_extract_dir_name)
     # load model and tokenizer
     model = AutoModelForSequenceClassification.from_pretrained(tarball_extract_dir_name)
     tokenizer = AutoTokenizer.from_pretrained(tarball_extract_dir_name)
